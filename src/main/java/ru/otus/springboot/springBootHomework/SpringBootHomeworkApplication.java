ff44c250ce8c8551f84ee943d4454d168b4b06ec
Anna Popova <anna.popova201909@gmail.com>
2021-06-15 12:58:23 +0300

    initial commit

---
+++ src/main/java/ru/otus/springboot/springBootHomework/SpringBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.springboot.springBootHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+
+}
